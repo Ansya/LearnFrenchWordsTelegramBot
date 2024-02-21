18a253dcfc8f9d1f4650a73645394563380915a9
ansya <anastasia.kuimova@gmail.com>
2024-02-21 11:14:24 +0300

    stage-3 menu created with exit and statistics items implemented

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,4 +21,28 @@ fun main() {
 
     println(dictionary)
 
+    do {
+        println("Меню:")
+        println("1 – Учить слова")
+        println("2 – Статистика")
+        println("0 – Выход")
+        println()
+        println("Введите номер выбранного пункта меню:")
+        val menuitem = readln()
+
+        when(menuitem) {
+            "1" -> println("1")
+            "2" -> println(getStatistics(dictionary))
+            "0" -> println("Выход")
+            else -> println("Введен неверный пункт меню, попробуйте еще раз")
+        }
+
+    } while (menuitem != "0")
+}
+
+fun getStatistics(dictionary : List<Word>) : String {
+    val learnedWordsCount = dictionary.filter {it.correctAnswersCount == 3}.count()
+    val allWordsCount = dictionary.count()
+    return "Выучено $learnedWordsCount из $allWordsCount слов | " +
+            "${(learnedWordsCount * 100) / allWordsCount}%"
 }
